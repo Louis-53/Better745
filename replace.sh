b85e7f627be7d23326046d4f111b2061e78168da
Louis-53 <149437491+Louis-53@users.noreply.github.com>
2024-10-24 20:03:28 +0330

    Add files via upload

---
+++ replace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Replacing new files..."
+sleep 2
+
+rm -f /var/www/html/wizwizxui-timebot/settings/values.php
+echo "Deleting values.php..."
+sleep 2
+
+curl -o /var/www/html/wizwizxui-timebot/settings/values.php https://raw.githubusercontent.com/Louis-53/Better745/main/settings/values.php
+echo "Replaced values.php."
+
+rm -f /var/www/html/wizwizxui-timebot/settings/warnusers.php
+echo "Deleting warnusers.php..."
+sleep 2
+
+curl -o /var/www/html/wizwizxui-timebot/settings/warnusers.php https://raw.githubusercontent.com/Louis-53/Better745/main/settings/warnusers.php
+echo "Replaced warnusers.php."
+
+echo "All files have been successfully replaced."
